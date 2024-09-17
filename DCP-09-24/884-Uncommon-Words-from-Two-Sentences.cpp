b9f07fa8656c4599dc672d813168c1f3a2c9dcd2
Praveen Kushwaha <118282436+KushwahaPraveen1@users.noreply.github.com>
2024-09-17 20:53:58 +0530

    [884] [Time Beats: 100.00%] [Memory Beats: 13.07%] - LeetPush

--- DCP-09-24/884-Uncommon-Words-from-Two-Sentences.cpp
+++ DCP-09-24/884-Uncommon-Words-from-Two-Sentences.cpp
@@ -1,64 +1,34 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        vector<string>vec1,vec2,ans;
-        string temp="";
-        for(auto it:s1)
-        {
+vector<string>vec1,vec2,ans;
+string temp="";
+for(auto it:s1)
+{
 if(it!=' ')
 temp+=it;
 else
+{vec1.push_back(temp);
+temp="";
+}}
+vec1.push_back(temp);temp="";
+for(auto it:s2)
 {
-    vec1.push_back(temp);
-    temp="";
-}
-        }
-        vec1.push_back(temp);temp="";
-         for(auto it:s2)
-        {
 if(it!=' ')temp+=it;
 else
 {
-    vec2.push_back(temp);
-    temp="";
+vec2.push_back(temp);
+temp="";
 }
-        }vec2.push_back(temp);temp="";
-
-// sort(vec1.begin(),vec1.end());
-// sort(vec2.begin(),vec2.end());
-
-// for(auto it:vec1)cout<<it<<" ";cout<<endl;
-// for(auto it:vec2)cout<<it<<" ";
-
+}
+vec2.push_back(temp);temp="";
 map<string,int>mp;
 for(int i=0;i<vec1.size();i++)
-{
-    mp[vec1[i]]++;
-}
+mp[vec1[i]]++; 
 for(int i=0;i<vec2.size();i++)
-{
-    mp[vec2[i]]++;
-}
+mp[vec2[i]]++; 
 for(auto it:mp)
-{
-    cout<<it.first<<" "<<it.second<<endl;
-    if(it.second==1)ans.push_back(it.first);
-}
-
-
-
-// vector<string>common;
-// unordered_set<string> st(vec1.begin(), vec1.end());
-// for(const auto& it:vec2)
-// {
-//     if(!(st.find(it)!=st.end()))
-//    { common.push_back(it);st.erase(it);}
-
- 
-// }  for(auto it:st)
-//    common.push_back(it); for (const auto& str : common) {
-//     cout << str <<" ";
-//     }
-    return ans;
+if(it.second==1)ans.push_back(it.first);
+return ans;
     }
 };
