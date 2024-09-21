8a00c0ab51c3864fa91f97a08e6e5a3e47378143
Praveen Kushwaha <118282436+KushwahaPraveen1@users.noreply.github.com>
2024-09-21 18:46:45 +0530

    [386] [Time Beats: 20.13%] [Memory Beats: 27.53%] - LeetPush

--- DCP-09-24/386-Lexicographical-Numbers.cpp
+++ DCP-09-24/386-Lexicographical-Numbers.cpp
@@ -4,15 +4,12 @@ class Solution {
 public:
     vector<int> lexicalOrder(int n) {
         vector<string>temp;
-        for(int i=1;i<=n;i++)
-        {
-temp.push_back(to_string(i));
-        }
+        for(int i=1;i<=n;i++) 
+        temp.push_back(to_string(i)); 
         sort(temp.begin(),temp.end());
         vector<int>ans;
         for(auto it:temp)
         ans.push_back(stoi(it));
-
         return ans;
     }
 };
